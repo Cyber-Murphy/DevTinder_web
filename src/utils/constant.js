9c6472fa2a5068f79103a76c0ab1b1412912f022
Gaurav Singh <gv00001singh@gmail.com>
2025-04-21 10:43:22 +0530

    created_dynamic_url_for_productionlevel_and_localhost

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,1 +1,9 @@
-export const BASE_URL="/api"
+
+// This is for production use
+// export const BASE_URL="/api"
+
+// This is for localhost use
+// export const BASE_URL='http://localhost:5000'
+
+// Now this will combine both,if we are using for localhost then this  use otherwise use the production level
+export const BASE_URL=location.hostname==="localhost"? 'http://localhost:5000':"/api"
